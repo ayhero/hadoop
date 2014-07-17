cfb2dbbad2d41806c87bc6ece2276fe5b776c199
ayhero <ayhero@yeah.net>
2014-07-17 16:48:56 +0800

    --增加了单机运行时的jar包

--- src/main/java/com/Local_Utils.java
+++ src/main/java/com/Local_Utils.java
@@ -11,6 +11,7 @@ public class Local_Utils {
 	public static void deleteLocalDir(String dir){
 		File file=new File(dir);
 		if(file.exists()){
+			//delete()方法不能删除非空文件夹，所以得用递归方式将file下所有包含内容删除掉，然后再删除file
 			if(file.isDirectory()){
 				File[] files=file.listFiles();
 				for(File f : files){
