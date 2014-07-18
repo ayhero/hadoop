5567cf4f8c40b4440366c626fc34c6001a665073
ayhero <ayhero@yeah.net>
2014-07-18 23:34:46 +0800

    --完善输出集合的所有子集的用例

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,13 +6,13 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums={1,2,3};
-		for(int i=1;i<Math.pow(2, 3);i++){
+		int[] nums={1,2,3,4};
+		for(int i=1;i<Math.pow(2, nums.length);i++){
 			String num=Integer.toBinaryString(i);
 			System.out.print(num+"--");
 			for(int j =num.length()-1;j>=0;j--){
 				if(num.charAt(j)=='1'){
-					System.out.print(nums[j]);
+					System.out.print(nums[num.length()-1-j]);
 				}
 			}
 			System.out.println();
